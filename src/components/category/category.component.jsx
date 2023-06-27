df668cf3ebceec8e98544841bbcdbb73e15f626f
aizazjk02 <56686849+aizazjk02@users.noreply.github.com>
2023-06-27 17:24:34 +0530

    fix : pop up not working on category page.

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -5,6 +5,7 @@ import {  useEffect, useState } from "react"
 import ProductCard from "../product-card/product-card.component"
 import { useSelector } from "react-redux"
 import { selectCategoriesMap } from "../../store/categories/categories.selectors"
+import PopUp from "../pop-up/pop-up.component"
 const Category = () => {
     const { category } = (useParams())
     console.log("🚀 ~ file: category.component.jsx:8 ~ Category ~ category:", category)
@@ -14,16 +15,21 @@ const Category = () => {
 
     const [products, setProducts] = useState([])
     // console.log("🚀 ~ file: category.component.jsx:13 ~ Category ~ products:", products)
-
+    const [showPopUp, setPopUp] = useState(false)
+    const popUpHandler = () => {
+        setPopUp(true)
+        setTimeout(() => { setPopUp(false) }, 1000)
+    }
     useEffect(() => {
         setProducts(categoriesMap[category])
     }, [categoriesMap, category])
 
     return (
         <>
+            {showPopUp ? <PopUp content={"Added To Cart!"}/> : ""}
             <h2 className="title">{category}</h2>
             <div className="category__container">
-                {products && products.map(product => <ProductCard key={product.id} product={product} />)}
+                {products && products.map(product => <ProductCard key={product.id} product={product} popUpHandler={popUpHandler}/>)}
             </div>
         </>
     )
